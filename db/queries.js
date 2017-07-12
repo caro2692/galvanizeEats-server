d2f95464aec00cfa6ce3b45ac96ccdf478a3c46e
Caroline O'Donnell <caroline.odonnell26@gmail.com>
2017-07-12 09:53:08 -0600

    updating query to remove duplicates

--- db/queries.js
+++ db/queries.js
@@ -7,7 +7,7 @@ module.exports = {
     ];
     return Promise.all(promises)
       .then(results=>{
-        const books = results[0];
+      const books = results[0];
         return Promise.all(
           books.map(book=>{
             return knex('book_author')
@@ -16,11 +16,12 @@ module.exports = {
             .where('book_id', book.id)
             .then(author_ids=>{
               book.authors = author_ids;
-              return books;
+              return book;
             });
           })
         );
       });
+
     //return knex('book').select('book_author.*', 'book.title', 'book.genre', 'book.description', 'book.cover_url', 'author.first_name', 'author.last_name', 'author.biography', 'author.portrait_url').innerJoin('book_author','book.id', 'book_author.book_id').innerJoin('author', 'author.id', 'book_author.author_id');
   },
   getAllAuthors(){
